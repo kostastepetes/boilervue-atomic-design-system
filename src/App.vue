4225973c237fc2c1070a1317ee479de1d3682b33
Kostas Tepetes <62772104+kostastepetes@users.noreply.github.com>
2022-10-25 15:07:34 +0300

    source code

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="container">
+    <NavBar />
+    <div class="content">
+      <router-view class="view" />
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppFooter from './components/organisms/AppFooter'
+import NavBar from './components/organisms/NavBar'
+
+export default {
+  name: 'app',
+  components: {
+    AppFooter,
+    NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+.container {
+  min-height: 95vh;
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  flex: 1;
+  display: flex;
+}
+.view {
+  width: 70%;
+  margin: 0 auto 0 auto;
+}
+</style>
